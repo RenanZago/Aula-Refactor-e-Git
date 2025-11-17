546f4b43a3c5d6ddde154130a0c9207b8480e044
zago <renanzago1308@gmail.com>
2025-11-16 23:30:34 -0300

    refactor: encapsulando campos de item

--- src/Item.java
+++ src/Item.java
@@ -1,7 +1,7 @@
 public class Item {
     public String name;
-    public int sellIn;
-    public int quality;
+    private int sellIn;
+    private int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -9,8 +9,16 @@ public class Item {
         this.quality = quality;
     }
 
-    @Override
-    public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+    public int getSellIn() {
+        return sellIn;
+    }
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+    public int getQuality() {
+        return quality;
+    }
+    public void setQuality(int quality) {
+        this.quality = quality;
     }
 }
